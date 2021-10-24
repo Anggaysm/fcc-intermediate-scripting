696fa8d47fb64df5ddba8b2e607e01ccbf74ad78
Anggaysm <prasetiaangga28@gmail.com>
2021-10-24 14:30:21 +0700

    add code

--- dna-pairing.js
+++ dna-pairing.js
@@ -6,7 +6,6 @@ function pairElement(str) {
             result.push(['C', 'G'])
         }else if( el === 'G'){
             result.push(['G', 'C'])
-            
         }else if(el === 'A'){
             result.push(['A', 'T'])
         }else if(el === 'T'){
@@ -20,3 +19,19 @@ function pairElement(str) {
   console.log(pairElement("GCG"));
   console.log(pairElement("ATCGA"))
   //[["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]]
+
+
+
+
+//second way
+const pairs = {
+    A : "T",
+    T : "A",
+    C : "G",
+    G : "C"
+}
+
+function pairElement2(str2){
+    return str2.split('').map(el => ([el, pairs[el]]))
+} 
+console.log(pairElement2("ATCGA"))
