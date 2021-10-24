d2efeb9520995d19abc70154852e302d5e71fbaf
Anggaysm <prasetiaangga28@gmail.com>
2021-10-24 17:42:46 +0700

    add code

--- sorted-union.js
+++ sorted-union.js
@@ -4,3 +4,7 @@ function uniteUnique(...arr) {
   
   console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
   //[1, 3, 2, 5, 4]
+  uniteUnique([1, 2, 3], [5, 2, 1])
+  //[1, 2, 3, 5]
+  uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8])
+  //[1, 2, 3, 5, 4, 6, 7, 8]
