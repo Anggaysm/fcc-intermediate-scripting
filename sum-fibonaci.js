f3eb1f2a34e8fc38e455c4e594ed19823af5b492
Anggaysm <prasetiaangga28@gmail.com>
2021-10-25 14:42:03 +0700

    add code

---
+++ sum-fibonaci.js
@@ -0,0 +1,21 @@
+//Sum All Odd Fibonacci Numbers
+function sumFibs(num) {
+    let f0 = 0,
+        f1 = 1,
+        next,
+        result = 0;
+    for(let i = 1; i <= num; i++){
+        if(f1 % 2 === 1 && f1<= num){
+            result += f1
+        }
+        next = f0 + f1
+        f0 = f1
+        f1 = next
+    }
+    return result
+  }
+  
+  console.log(sumFibs(1000));
+  //1785
+  sumFibs(4000000)
+  //4613732
