dd123db1f730278c9abf01a0820905d57d588887
Anggaysm <prasetiaangga28@gmail.com>
2021-10-26 21:42:03 +0700

    add code

---
+++ add-together.js
@@ -0,0 +1,20 @@
+function addTogether(...args) {
+    for(let i = 0; i < args.length; i++){
+        if(typeof args[i] != 'number'){
+            return undefined
+        }
+    }
+    if(args.length == 1){
+        return(angka2) =>{
+            if(typeof angka2 != 'number'){
+                return undefined
+            }
+            return args[0] + angka2
+        }
+    }
+    return args[0]+args[1]
+  }
+  
+  console.log(addTogether(2, 3));
+  console.log(addTogether("https://www.youtube.com/watch?v=dQw4w9WgXcQ"))
+  console.log(addTogether(5)(7))
